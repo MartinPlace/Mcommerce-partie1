455e87c051b8d96ac2222ee979435e10cad0eebe
Martin <place.martin@gmail.com>
2018-12-02 09:01:20 +0100

    premiere partie

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -16,7 +16,7 @@ public class Product {
     @GeneratedValue
     private int id;
 
-    @Length(min=3, max=20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
+    @Length(min=3, max=20, message = "Le nom doit être compris entre 3 et 20 caractères max.")
     private String nom;
 
     @Min(value = 1)
